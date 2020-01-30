0e2c364f309bb8311fe40d13616f81b875cd9b16
Lanre039 <rajilat1000@gmail.com>
2020-01-30 12:31:52 +0100

    added new navbar component

---
+++ CLIENT/src/app/new-nav/new-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-nav',
+  templateUrl: './new-nav.component.html',
+  styleUrls: ['./new-nav.component.scss']
+})
+export class NewNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
